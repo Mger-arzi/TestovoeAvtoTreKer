c1c994edfe2d094e46c075c6d7a7b656e4f76fdf
Mger-arzi <mger54598@gmail.com>
2024-07-02 10:15:19 +0300

    add button

---
+++ autoTrek/src/components/ui/button/button.tsx
@@ -0,0 +1,28 @@
+import { ComponentPropsWithoutRef, ElementType, ReactNode } from 'react'
+
+import s from './button.module'
+
+export type ButtonProps<T extends ElementType> = {
+  as?: T
+  children: ReactNode
+  className?: string
+  fullWidth?: boolean
+  variant?: 'primary' | 'secondary'
+} & ComponentPropsWithoutRef<T>
+
+export const Button = <T extends ElementType>({
+  as,
+  className = `${s.button}`,
+  fullWidth,
+  variant = 'primary',
+  ...restProps
+}: ButtonProps<T>) => {
+  const Component = as ?? 'button'
+
+  return (
+    <Component
+      className={`${s[variant]} ${fullWidth ? s.fullWidth : ''} ${className}`}
+      {...restProps}
+    />
+  )
+}
